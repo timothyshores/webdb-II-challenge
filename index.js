1371b16dad0d476841ddd778507bf198d974c0cc
Timothy Shores <timothymshores@gmail.com>
2019-04-30 16:53:02 -0500

    Create router.js and configure knex to connect to /data/lambda.sqlite3. Import router into index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const server = express();
+const router = require('./router');
 
+const server = express();
 server.use(express.json());
 server.use(helmet());
+
+server.use('/api/zoos', router);
 
 // endpoints here
 
 const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+server.listen(port, () => {
+    console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
