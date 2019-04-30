000d311c8ec8b43570f4236e54ecfd44388a98f7
Timothy Shores <timothymshores@gmail.com>
2019-04-30 17:33:23 -0500

    Created POST request endpoint to /api/bears/

--- routes/bearsRouter.js
+++ routes/bearsRouter.js
@@ -32,4 +32,11 @@ router.get('/:id', (req, res) => {
         .catch(err => res.status(500).json(err));
 });
 
+router.post('/', (req, res) => {
+    db('bears')
+        .insert(req.body, 'id')
+        .then(results => res.status(200).json(results))
+        .catch(err => res.status(500).json(err));
+});
+
 module.exports = router;
